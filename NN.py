e2f583a34e2041677e4e51514f8db338b489f415
shehabeldeenibrahim <shehab_eldiin@hotmail.com>
2020-04-24 04:27:03 +0200

    changed to matrix

--- NN.py
+++ NN.py
@@ -22,7 +22,7 @@ class Layer:
         self.delWeight = np.zeros([nOutputs, nInputs + 1])
         self.momentum = momentum
         # insert random weights including bias
-        self.weightsArray = np.random.uniform(low=0.1, high=0.1, size=(nOutputs, nInputs + 1))
+        self.weightsArray = np.random.uniform(low=-0.05, high=0.05, size=(nOutputs, nInputs + 1))
         
     # Forward Propagation method
     def forwardProp(self):
@@ -36,37 +36,28 @@ class Layer:
     # Compute Softmax to get PDF
     def softmax(self):
         self.predictions = softmax(self.activations)
-
     # Back Propagation method
     # Update weights
     # Compute delta for the output layer
     def computeDeltaOutput(self):
-        for k in range(self.activations.size):
-            self.deltaOutputs[k] =  self.activations[k] * (1-self.activations[k]) * (self.target[k] - self.activations[k])
+        self.deltaOutputs =  self.activations * (1-self.activations) * (self.target - self.activations)
 
     # Compute delta for the hidden layer
     def computeDeltaHidden(self, outputLayer):
         # loop on hidden nodes without bias and calculate 
         # delta with respect to output layer delta
-        for j in range(self.activations.size - 1):
-            sumK = 0
-            for k in range(outputLayer.activations.size):
-                sumK += outputLayer.weightsArray[k][j] * outputLayer.deltaOutputs[k]
-            self.deltaHidden[j] = self.activations[j] * (1 - self.activations[j]) * sumK     
+        self.deltaHidden = self.activations * (1 - self.activations) * np.dot(np.transpose(outputLayer.weightsArray), outputLayer.deltaOutputs)      
 
     # Compute delta weights in the outputLayer and update weights
     def updateOutputWeights(self, hiddenLayer):
-        for k in range(self.activations.size):
-            for j in range(hiddenLayer.activations.size):
-                self.delWeight[k][j] = self.step * self.deltaOutputs[k] * hiddenLayer.activations[j] + momentum * self.delWeight[k][j]
+        self.delWeight = self.step * np.transpose(np.dot(np.transpose([hiddenLayer.activations]), [self.deltaOutputs])) + momentum * self.delWeight
         self.weightsArray += self.delWeight
 
     # Compute weights in hidden layer and update weights
     def updateHiddenWeights(self):
-        for j in range(self.activations.size - 1):
-            for i in range(self.inputNodes.size):
-                self.delWeight[j][i] = self.step * self.deltaHidden[j] * self.inputNodes[i] + momentum * self.delWeight[j][i]
+        self.delWeight = self.step * np.dot(np.transpose([self.deltaHidden[:-1]]), [self.inputNodes]) + momentum * self.delWeight
         self.weightsArray += self.delWeight
+
     # Input nodes setter
     def setInput(self, inputNodes):
         self.inputNodes = inputNodes
@@ -82,6 +73,78 @@ class Layer:
     def addBiasActivations(self):
         self.activations = np.append(self.activations, 1)
     
+def train(inputImg, target ,hiddenLayer, outputLayer):
+    # Set the target value tk for output unit k to 0.9 if the input class is the kth class, 0.1 otherwise.
+    labelNormalized = np.where(target > 0, 0.9, 0.1)
+    outputLayer.setTarget(labelNormalized)
+
+    # Set input to hidden layer
+    hiddenLayer.setInput(inputImg)
+
+    # Add bias node to hidden layer
+    hiddenLayer.addBiasInput()
+
+    # Compute hidden layer activations
+    hiddenLayer.forwardProp()
+
+    # Add Bias on activations
+    hiddenLayer.addBiasActivations()
+
+    # Compute output layer activiations
+    outputLayer.setInput(hiddenLayer.activations)
+
+    outputLayer.forwardProp()
+
+    # Compute deltaK
+    outputLayer.computeDeltaOutput()
+
+    # Compute deltaJ
+    hiddenLayer.computeDeltaHidden(outputLayer)
+
+    # Update Output weights
+    outputLayer.updateOutputWeights(hiddenLayer)
+
+    # Update Hidden weights
+    hiddenLayer.updateHiddenWeights()
+
+def getAccuracy(trainImgs, trainLabels, hiddenLayer, outputLayer, confusionActual, confusionPrediction):
+    Trues = 0
+    confusionPrediction = []
+    confusionActual = []
+    for img in range(trainImgs.shape[0]):
+
+        # Set input to hidden layer
+        hiddenLayer.setInput(trainImgs[img])
+
+        # Add bias node to hidden layer
+        hiddenLayer.addBiasInput()
+
+        # Compute hidden layer activations
+        hiddenLayer.forwardProp()
+
+        # Add Bias on activations
+        hiddenLayer.addBiasActivations()
+
+        # Compute output layer activiations
+        outputLayer.setInput(hiddenLayer.activations)
+
+        outputLayer.forwardProp()
+
+        # Softmax
+        outputLayer.softmax()
+
+        # Calculate Accuracy
+        prediction = np.argmax(outputLayer.predictions)
+
+        actual = np.argmax(trainLabels[img]).astype(int)
+
+        if(prediction == actual):
+            Trues += 1
+        confusionPrediction.append(prediction)
+
+        confusionActual.append(actual)
+
+    return Trues / trainImgs.shape[0]
 
 
 
@@ -92,8 +155,8 @@ picture_train = dataset_train.iloc[0:15000,1:785].values
 
 # Importing test dataset
 dataset_test = pd.read_csv('test.csv')
-labels_test = dataset_test.iloc[:, 0].values
-picture_test = dataset_test.iloc[:,1:785].values
+labels_test = dataset_test.iloc[0:1000, 0].values
+picture_test = dataset_test.iloc[0:1000,1:785].values
 
 # Scale inputs
 picture_train = picture_train/255
@@ -112,45 +175,29 @@ labels_test = ohe.transform(labels_test)
 labels_test = labels_test.toarray()
 
 # Number of hidden nodes
-n = 2
-step = 0.2
+n = 20
+step = 0.1
 momentum = 0.9
-picture_train = [[1,0]]
-label_train = [[0.9]]
+
 # Initialize Layers
-hiddenLayer = Layer(picture_train[0], 2, n, step, momentum)
-outputLayer = Layer(picture_train[0], n , 1, step, momentum)
-outputLayer.setTarget(label_train[0])
+hiddenLayer = Layer(picture_train[0], 784, n, step, momentum)
+outputLayer = Layer(picture_train[0], n , 10, step, momentum)
 
-# Set input to hidden layer
-hiddenLayer.setInput(picture_train[0])
+# Arrays storing accuracies
+trainAccuracyArray = []
+testAccuracyArray = []
+confusionPrediction = []
+confusionActual = []
 
-# Add bias node to hidden layer
-hiddenLayer.addBiasInput()
+epochs = 10
+for epoch in range(0, epochs):
+    trainTrues = 0
+    for img in range(picture_train.shape[0]):
+        train(picture_train[img], label_train[img], hiddenLayer, outputLayer)
 
-# Compute hidden layer activations
-hiddenLayer.forwardProp()
-
-# Add Bias on activations
-hiddenLayer.addBiasActivations()
-
-# Compute output layer activiations
-outputLayer.setInput(hiddenLayer.activations)
-
-outputLayer.forwardProp()
-
-# Compute deltaK
-outputLayer.computeDeltaOutput()
-
-# Compute deltaJ
-hiddenLayer.computeDeltaHidden(outputLayer)
-
-# Update Output weights
-outputLayer.updateOutputWeights(hiddenLayer)
-
-# Update Hidden weights
-hiddenLayer.updateHiddenWeights()
-
-# Compute softmax
-outputLayer.softmax()
+    # first epoch training done on all train data  
+    # get train and test Accuracy  
+    trainAccuracy = getAccuracy(picture_train, label_train,hiddenLayer, outputLayer, confusionActual, confusionPrediction)    
+    testAccuracy = getAccuracy(picture_test, labels_test, hiddenLayer, outputLayer, confusionActual, confusionPrediction)
+    print("Epoch #" + str(epoch+1) + ": train accuracy: " + str(trainAccuracy)+ ": test accuracy: " + str(testAccuracy))
 print("End")
